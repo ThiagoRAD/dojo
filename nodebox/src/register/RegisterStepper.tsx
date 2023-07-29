d56ff4ec3e68f5bb9f267553edf1f3aa81c07f99
ThiagoRAD <thauroo@gmail.com>
2023-07-29 17:46:15 -0300

    stepper

--- nodebox/src/register/RegisterStepper.tsx
+++ nodebox/src/register/RegisterStepper.tsx
@@ -1,10 +1,11 @@
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../store';
-import { Step } from './Step'
-import RegisterAccount from './RegisterAccount'
-import RegisterAddress from './RegisterAddress'
-import RegisterPayment from './RegisterPayment'
-import { nextStep, prevStep } from './registerSlice'
+import {Step} from './Step';
+import RegisterAccount from './RegisterAccount';
+import RegisterAddress from './RegisterAddress';
+import RegisterPayment from './RegisterPayment';
+import {nextStep, prevStep} from './registerSlice';
+import './RegisterStepper.css';
 
 const RegisterStepper = () => {
   const {currentStep, steps} = useSelector((state: RootState) => state.register);
@@ -21,19 +22,26 @@ const RegisterStepper = () => {
   };
 
   return (
-    <div>
+    <div className='stepper-container'>
       <div>
-        {steps.map((step) => (
-          <div key={step} className={`step ${step === currentStep && 'active'}`}>
-            {step}
-          </div>
-        ))}
+        <div className='stepper-navigation'>
+          {steps.map((step, index) => (
+            <div key={step} className='step'>
+              <div className={`step-circle ${step === currentStep && 'active'}`}>{index+1}</div>
+              <div className={`step-label ${step === currentStep && 'active'}`}>{step}</div>
+            </div>
+          ))}
+        </div>
         {currentStep === Step.Account && <RegisterAccount />}
         {currentStep === Step.Address && <RegisterAddress />}
         {currentStep === Step.Payment && <RegisterPayment />}
-        <div className="navigation">
-          <button disabled={isFirstStep} onClick={handleBack}>Back</button>
-          <button disabled={isLastStep} onClick={handleNext}>Next</button>
+        <div className='navigation'>
+          <button disabled={isFirstStep} onClick={handleBack}>
+            Back
+          </button>
+          <button disabled={isLastStep} onClick={handleNext}>
+            Next
+          </button>
         </div>
       </div>
     </div>
